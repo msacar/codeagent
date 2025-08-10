842b2a6ff6ff5f8b79ed6159a3c9022700ac0be4
Mustafa Acar <mstkmstkk@hotmail.com>
2025-08-10 14:13:39 +0300

    def ref

--- codeagent/queries/tree-sitter-language-pack/javascript-tags.scm
+++ codeagent/queries/tree-sitter-language-pack/javascript-tags.scm
@@ -1,93 +1,88 @@
-; JavaScript and JSX tags for code navigation and context
-; Captures definitions that can be used for code indexing
+(
+  (comment)* @doc
+  .
+  (method_definition
+    name: (property_identifier) @name.definition.method) @definition.method
+  (#not-eq? @name.definition.method "constructor")
+  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
+  (#select-adjacent! @doc @definition.method)
+)
 
-; Classes
-(class_declaration
-  name: (identifier) @name.definition.class)
+(
+  (comment)* @doc
+  .
+  [
+    (class
+      name: (_) @name.definition.class)
+    (class_declaration
+      name: (_) @name.definition.class)
+  ] @definition.class
+  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
+  (#select-adjacent! @doc @definition.class)
+)
 
-; Functions
-(function_declaration
-  name: (identifier) @name.definition.function)
+(
+  (comment)* @doc
+  .
+  [
+    (function_expression
+      name: (identifier) @name.definition.function)
+    (function_declaration
+      name: (identifier) @name.definition.function)
+    (generator_function
+      name: (identifier) @name.definition.function)
+    (generator_function_declaration
+      name: (identifier) @name.definition.function)
+  ] @definition.function
+  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
+  (#select-adjacent! @doc @definition.function)
+)
 
-; Arrow functions assigned to variables
-(lexical_declaration
-  (variable_declarator
-    name: (identifier) @name.definition.function
-    value: (arrow_function)))
-
-; Function expressions assigned to variables
-(lexical_declaration
-  (variable_declarator
-    name: (identifier) @name.definition.function
-    value: (function_expression)))
-
-; Methods in classes
-(method_definition
-  name: (property_identifier) @name.definition.method)
-
-; Properties in classes
-(field_definition
-  property: (property_identifier) @name.definition.property)
-
-; Getters
-(method_definition
-  name: (property_identifier) @name.definition.getter
-  (#match? @name.definition.getter "^get"))
-
-; Setters
-(method_definition
-  name: (property_identifier) @name.definition.setter
-  (#match? @name.definition.setter "^set"))
-
-; Constructors
-(method_definition
-  name: (property_identifier) @name.definition.constructor
-  (#eq? @name.definition.constructor "constructor"))
-
-; Variables/Constants (top-level)
-(lexical_declaration
-  (variable_declarator
-    name: (identifier) @name.definition.variable))
-
-; Variable declarations
-(variable_declaration
-  (variable_declarator
-    name: (identifier) @name.definition.variable))
-
-; Object property assignments (module pattern)
-(assignment_expression
-  left: (member_expression
-    object: (identifier) @name.reference.object
-    property: (property_identifier) @name.definition.property))
-
-; Export statements
-(export_statement
-  declaration: (lexical_declaration
+(
+  (comment)* @doc
+  .
+  (lexical_declaration
     (variable_declarator
-      name: (identifier) @name.definition.export)))
+      name: (identifier) @name.definition.function
+      value: [(arrow_function) (function_expression)]) @definition.function)
+  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
+  (#select-adjacent! @doc @definition.function)
+)
 
-; Named exports
-(export_statement
-  (export_clause
-    (export_specifier
-      name: (identifier) @name.definition.export)))
+(
+  (comment)* @doc
+  .
+  (variable_declaration
+    (variable_declarator
+      name: (identifier) @name.definition.function
+      value: [(arrow_function) (function_expression)]) @definition.function)
+  (#strip! @doc "^[\\s\\*/]+|^[\\s\\*/]$")
+  (#select-adjacent! @doc @definition.function)
+)
 
-; Import statements
-(import_statement
-  (import_clause
-    (named_imports
-      (import_specifier
-        name: (identifier) @name.reference.import))))
+(assignment_expression
+  left: [
+    (identifier) @name.definition.function
+    (member_expression
+      property: (property_identifier) @name.definition.function)
+  ]
+  right: [(arrow_function) (function_expression)]
+) @definition.function
 
-; JSX Components (capitalized identifiers in JSX)
-(jsx_opening_element
-  name: (identifier) @name.reference.component
-  (#match? @name.reference.component "^[A-Z]"))
+(pair
+  key: (property_identifier) @name.definition.function
+  value: [(arrow_function) (function_expression)]) @definition.function
 
-(jsx_closing_element
-  name: (identifier) @name.reference.component
-  (#match? @name.reference.component "^[A-Z]"))
+(
+  (call_expression
+    function: (identifier) @name.reference.call) @reference.call
+  (#not-match? @name.reference.call "^(require)$")
+)
 
-(jsx_self_closing_element
-  name: (identifier) @name.reference.component
-  (#match? @name.reference.component "^[A-Z]"))
+(call_expression
+  function: (member_expression
+    property: (property_identifier) @name.reference.call)
+  arguments: (_) @reference.call)
+
+(new_expression
+  constructor: (_) @name.reference.class) @reference.class
--- codeagent/queries/tree-sitter-language-pack/python-tags.scm
+++ codeagent/queries/tree-sitter-language-pack/python-tags.scm
@@ -1,138 +1,14 @@
-; Python tags for code navigation and context
-; Captures definitions that can be used for code indexing
+(module (expression_statement (assignment left: (identifier) @name.definition.constant) @definition.constant))
 
-; Classes
 (class_definition
-  name: (identifier) @name.definition.class)
+  name: (identifier) @name.definition.class) @definition.class
 
-; Functions
 (function_definition
-  name: (identifier) @name.definition.function)
+  name: (identifier) @name.definition.function) @definition.function
 
-; Methods (functions inside classes)
-(class_definition
-  body: (block
-    (function_definition
-      name: (identifier) @name.definition.method)))
-
-; Async functions
-(function_definition
-  "async" @keyword
-  name: (identifier) @name.definition.async_function)
-
-; Decorated functions
-(decorated_definition
-  (decorator) @decorator
-  definition: (function_definition
-    name: (identifier) @name.definition.function))
-
-; Decorated classes
-(decorated_definition
-  (decorator) @decorator
-  definition: (class_definition
-    name: (identifier) @name.definition.class))
-
-; Lambda functions assigned to variables
-(assignment
-  left: (identifier) @name.definition.variable
-  right: (lambda))
-
-; Global variables (top-level assignments)
-(module
-  (expression_statement
-    (assignment
-      left: (identifier) @name.definition.variable)))
-
-; Type aliases (Python 3.10+)
-(type_alias
-  name: (identifier) @name.definition.type)
-
-; Import statements
-(import_statement
-  name: (dotted_name
-    (identifier) @name.reference.import))
-
-(import_from_statement
-  name: (dotted_name
-    (identifier) @name.reference.import))
-
-; Imported names
-(import_from_statement
-  (aliased_import
-    name: (identifier) @name.reference.import
-    alias: (identifier) @name.definition.import_alias))
-
-(import_from_statement
-  name: (identifier) @name.reference.import)
-
-; Function parameters (useful for understanding signatures)
-(parameters
-  (identifier) @name.definition.parameter)
-
-(default_parameter
-  name: (identifier) @name.definition.parameter)
-
-(typed_parameter
-  (identifier) @name.definition.parameter)
-
-(typed_default_parameter
-  name: (identifier) @name.definition.parameter)
-
-; Dataclass-like decorators
-(decorated_definition
-  (decorator
-    (identifier) @decorator_name
-    (#match? @decorator_name "^(dataclass|dataclasses\\.dataclass|attr\\.s|attrs\\.define)$"))
-  definition: (class_definition
-    name: (identifier) @name.definition.dataclass))
-
-; Property decorators
-(decorated_definition
-  (decorator
-    (attribute
-      attribute: (identifier) @decorator_name
-      (#match? @decorator_name "^(property|cached_property)$")))
-  definition: (function_definition
-    name: (identifier) @name.definition.property))
-
-; Class attributes (type annotations in class body)
-(class_definition
-  body: (block
-    (expression_statement
-      (assignment
-        left: (identifier) @name.definition.attribute))))
-
-; Global constants (UPPER_CASE variables)
-(module
-  (expression_statement
-    (assignment
-      left: (identifier) @name.definition.constant
-      (#match? @name.definition.constant "^[A-Z_]+$"))))
-
-; Exception definitions
-(class_definition
-  name: (identifier) @name.definition.exception
-  superclasses: (argument_list
-    (identifier) @superclass
-    (#match? @superclass "^(Exception|Error|Warning)$")))
-
-; Context managers (with statements)
-(with_statement
-  (with_clause
-    (with_item
-      value: (identifier) @name.reference.context_manager)))
-
-; Comprehensions
-(list_comprehension
-  body: (identifier) @name.reference.variable)
-
-(dictionary_comprehension
-  body: (pair
-    key: (identifier) @name.reference.key
-    value: (identifier) @name.reference.value))
-
-(set_comprehension
-  body: (identifier) @name.reference.variable)
-
-(generator_expression
-  body: (identifier) @name.reference.variable)
+(call
+  function: [
+      (identifier) @name.reference.call
+      (attribute
+        attribute: (identifier) @name.reference.call)
+  ]) @reference.call
